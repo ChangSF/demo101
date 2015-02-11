ac12ade9c0a4abf8a7cebacc3256c433d03954a1
CSF_Chang <qq952327293@gmail.com>
2015-02-11 18:03:14 +0800

    pp

---
+++ Assets/_Scripts/Logical/_CommonGame/PropManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SuperHero.Logical
+{
+	public class PropManager {
+
+		public GameObject PrefabSpeedUp;
+
+		public Queue<GameObject> QueueSpeedup;
+
+		public GameObject PrefabGroupAttack;
+
+		public Queue <GameObject> QueueGroupAttack;
+
+		public GameObject PrefabRecoverHP;
+
+		public Queue<GameObject> QueueRecoverHP;
+
+		public GameObject PrefabMine;
+
+		public Queue<GameObject> QueueMine;
+
+
+
+
+
+
+	}
+}
