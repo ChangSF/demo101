4dff5c0720ed9cf51111dfef030ea457b262ce0e
CSF952327293 <chang@ShanFudeMacBook-Pro.local>
2015-04-24 11:22:59 +0800

    climbTrigger
    climbTrigger

---
+++ Assets/_Scripts/Logical/_CommonGame/Trigger/climbTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class climbTrigger: MonoBehaviour 
+{
+	public Vector3 direction;
+
+
+
+	void OnTriggerEnter(Collider  other)
+	{
+		GlobalInGame.currentPC.Climbing(direction);
+
+	}
+}
